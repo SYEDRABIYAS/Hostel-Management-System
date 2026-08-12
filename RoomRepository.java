42b44db43db01545aa013542f3d8547e88b589b9
SYEDRABIYAS <syeds.ug.24.it@francisxavier.ac.in>
2026-08-12 19:58:08 +0530

    Delete RoomRepository.java

--- RoomRepository.java
+++
@@ -1,8 +0,0 @@
-package com.hostel.hostelmanagement.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.hostel.hostelmanagement.entity.Room;
-
-public interface RoomRepository extends JpaRepository<Room, Long> {
-
-}
