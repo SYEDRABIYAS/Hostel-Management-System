16c6dfb447ef199877f15664fe280ef1b990622c
SYEDRABIYAS <syeds.ug.24.it@francisxavier.ac.in>
2026-08-12 19:54:08 +0530

    Delete FeeRepository.java

--- FeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.hostel.hostelmanagement.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.hostel.hostelmanagement.entity.Fee;
-
-public interface FeeRepository extends JpaRepository<Fee, Long> {
-
-}
